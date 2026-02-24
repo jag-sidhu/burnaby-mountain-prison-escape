ef3d34386d0e79e7e82cc79b72873a088ec6ceda
nick zhou <nickz@MacBook-Air-228.local>
2026-02-23 17:48:44 -0800

    Update Player.java to handle movement and reset

--- src/main/java/Spring2026Team10/Player.java
+++ src/main/java/Spring2026Team10/Player.java
@@ -1,11 +1,27 @@
 package Spring2026Team10;
 
 public class Player extends Entity {
-    public void update() {
-        // Placeholder for upcoming movement/input logic.
+
+    public Player(int startX, int startY, PrisonMap map) {
+        super(startX, startY, map);
+    }
+
+    public void update(KeyHandler keyHandler) {
+        //currently can't move diagonally on the tiles, can change it if we want player to be able to do so
+        if (keyHandler.upPressed) {
+            move(0, -1);
+        } else if (keyHandler.downPressed) {
+            move(0, 1);
+        } else if (keyHandler.leftPressed) {
+            move(-1, 0);
+        } else if (keyHandler.rightPressed) {
+            move(1, 0);
+        }
+
     }
 
     public void reset() {
-        // Placeholder for upcoming spawn/reset logic.
+        this.x = map.getStartTile().x;
+        this.y = map.getStartTile().y;
     }
 }
