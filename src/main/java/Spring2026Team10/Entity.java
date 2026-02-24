5403b967dd22bd1a48a64ee66bfa91fc4742b4de
nick zhou <nickz@MacBook-Air-228.local>
2026-02-23 18:38:31 -0800

    Update Player.java and Entity.java, smoothed the movement, powerups

--- src/main/java/Spring2026Team10/Entity.java
+++ src/main/java/Spring2026Team10/Entity.java
@@ -2,16 +2,22 @@ package Spring2026Team10;
 
 public abstract class Entity {
     //main entity logic, player and guard will extend this
+    //tile position
     protected int x; //column
     protected int y; //row
 
-    protected int speed = 1; // default speed can be overwritten by subclass, 1 for 1 tile
+    protected double posX; //for smoother movement
+    protected double posY; //for smoother movement
+
+    protected int speed = 1; // default number of tiles per frame
 
     protected PrisonMap map;
 
-    public Entity(int StartX, int StartY, PrisonMap map) {
-        this.x = StartX;
-        this.y = StartY;
+    public Entity(int startX, int startY, PrisonMap map) {
+        this.x = startX;
+        this.y = startY;
+        this.posX = startX;
+        this.posY = startY;
         this.map = map;
     }
     //getter methods so that only Guard and Player can change the x and y values
@@ -21,15 +27,32 @@ public abstract class Entity {
     public int getY() {
         return y;
     }
-    //movement
-    public void move(int dx, int dy) {
-        int newX = x + dx * speed;
-        int newY = y + dy * speed;
 
-        if (map.isWalkable(newX,newY)) {
-            this.x = newX;
-            this.y = newY;
+    //movement
+    public void move(double dx, double dy) {
+        double remainingX = dx * speed;
+        double remainingY = dy * speed;
+
+        // Move step by step in small increments (0.1 tile increments)
+        double step = 0.1;
+        while (Math.abs(remainingX) > 0 || Math.abs(remainingY) > 0) {
+            double stepX = Math.min(step, Math.abs(remainingX)) * Math.signum(remainingX);
+            double stepY = Math.min(step, Math.abs(remainingY)) * Math.signum(remainingY);
+
+            double newPosX = posX + stepX;
+            double newPosY = posY + stepY;
+
+            if (map.isWalkable((int)newPosY, (int)newPosX)) {
+                posX = newPosX;
+                posY = newPosY;
+                x = (int) posX;
+                y = (int) posY;
+            } else {
+                break; // stop at wall
+            }
+
+            remainingX -= stepX;
+            remainingY -= stepY;
         }
     }
-
 }
--- src/main/java/Spring2026Team10/Player.java
+++ src/main/java/Spring2026Team10/Player.java
@@ -2,26 +2,82 @@ package Spring2026Team10;
 
 public class Player extends Entity {
 
+    //player's stats
+    private int lives = 3;
+    private int score = 0;
+
+    //when he grabs the powerups
+    private boolean speedBoostActive = false;
+    private int speedBoostDuration = 0;
+
     public Player(int startX, int startY, PrisonMap map) {
         super(startX, startY, map);
     }
 
+    //called each frame
     public void update(KeyHandler keyHandler) {
+        //checks the powerups
+        updatePowerUps();
+        double dx = 0, dy = 0;
         //currently can't move diagonally on the tiles, can change it if we want player to be able to do so
-        if (keyHandler.upPressed) {
-            move(0, -1);
-        } else if (keyHandler.downPressed) {
-            move(0, 1);
-        } else if (keyHandler.leftPressed) {
-            move(-1, 0);
-        } else if (keyHandler.rightPressed) {
-            move(1, 0);
-        }
+        if (keyHandler.upPressed) dy = -1; 
+        else if (keyHandler.downPressed) dy = 1;
+        else if (keyHandler.leftPressed) dx = -1;
+        else if (keyHandler.rightPressed) dx = 1;
+
+        move(dx, dy); //moves the player based on the input and speed
+        
 
     }
-
+    //resets the player to the starting point
     public void reset() {
-        this.x = map.getStartTile().x;
-        this.y = map.getStartTile().y;
+        posX = map.getStartTile().x;
+        posY = map.getStartTile().y;
+        x = (int) posX;
+        y = (int) posY;
+        
+        lives = 3;
+        score = 0;
+        speedBoostActive = false;
+        speedBoostDuration = 0;
+        speed = 1;
+    }
+
+    //collisions with guards
+    public boolean collidesWithGuard(Guard guard) {
+        return this.x == guard.getX() && this.y == guard.getY();
+    }
+
+    public void activateSpeedBoost(int duration) {
+        speedBoostActive = true;
+        speedBoostDuration = duration;
+        speed = 2; // double speed while active
+    }
+
+    private void updatePowerUps() {
+        if (speedBoostActive) {
+            speedBoostDuration--;
+            if (speedBoostDuration <= 0) {
+                speedBoostActive = false;
+                speed = 1; // reset to normal speed
+            }
+        }
+    }
+
+    // Player lives and score
+    public int getLives() {
+        return lives;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void loseLife() {
+        lives--;
+    }
+
+    public void gainScore(int amount) {
+        score += amount;
     }
 }
