3b256ee20acb4c32d9aac5eb8ef9a77a35415f20
Arsh Behniwal <behniwal.arsh@gmail.com>
2026-03-10 09:10:38 -0700

    Updated wall sprite for better visibility. Javadoc

--- src/main/java/Spring2026Team10/Main.java
+++ src/main/java/Spring2026Team10/Main.java
@@ -2,6 +2,10 @@ package Spring2026Team10;
 
 import javax.swing.JFrame;
 
+/**
+ * Main function of the game. Constructs a new game instance to run.
+ *
+ */
 public class Main {
     public static void main(String[] args) {
       GameConstructor game = new GameConstructor();
