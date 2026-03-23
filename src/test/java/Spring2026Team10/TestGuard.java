8f576711b72badad6856fc34fd88b93e607bfbfb
nick zhou <nickz@MacBook-Air-228.local>
2026-03-23 00:21:36 -0700

    Add guard test - constructor sets CHASE state

--- src/test/java/Spring2026Team10/TestGuard.java
+++ src/test/java/Spring2026Team10/TestGuard.java
@@ -12,4 +12,12 @@ public class TestGuard {
         assertEquals(Guard.GuardState.PATROLLING, guard.getState(),
                 "PATROL type guard should start in PATROLLING state");
     }
+
+    @Test
+    public void testGuardConstructorSetsChaseStateForChaseType() {
+        PrisonMap map = new PrisonMap();
+        Guard guard = new Guard(50, 35, map, Guard.GuardType.CHASE, true, 10);
+        assertEquals(Guard.GuardState.CHASING, guard.getState(),
+                "CHASE type guard should start in CHASING state");
+}
 }
